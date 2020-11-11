26d998670590539a2a7a67449b7b6d816d81f6bb
irvan wijaya <irfanwijayasardam@gmail.com>
2020-11-12 02:20:23 +0700

    merge frontend and backend

---
+++ app/src/main/java/com/aminivan/mapslearn/MainMaps.kt
@@ -0,0 +1,11 @@
+package com.aminivan.mapslearn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMaps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_maps)
+    }
+}
